408a8ad3f8a2d6e051ca890fa39dda26936fab36
unknown <josecolomerhe2@gmail.com>
2022-06-22 10:21:17 +0200

    Simular databae con un fichero JSON

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-$contacts =[
-    ["name"=>"Pepe", "phone_number"=>"2132139"],
-    ["name"=>"Jose", "phone_number"=>"2132134"],
-    ["name"=>"Nate", "phone_number"=>"1234456"]
-
-
-];
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+}else{
+  $contacts = [];
+}
 
 
 
@@ -62,7 +60,7 @@ $contacts =[
             <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./add.html">Add Contact</a>
+            <a class="nav-link" href="./add.php">Add Contact</a>
           </li>
         </ul>
       </div>
@@ -72,7 +70,16 @@ $contacts =[
   <main>
     <div class="container pt-4 p-3">
       <div class="row">
+      <?php if(count($contacts)== 0):?>
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
+            </div>
+          </div>
+      <?php endif?>
       <?php foreach ($contacts as $contact) :?>
+      
         <div class="col-md-4 mb-3">
           <div class="card text-center">
             <div class="card-body">
